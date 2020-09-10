a773c9c5a133bfd08a1cc2649f9193ba3722ac46
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-09-10 16:15:18 +0200

    Refaktoring

--- ITeam.DotnetCore.WebApi/Startup.cs
+++ ITeam.DotnetCore.WebApi/Startup.cs
@@ -1,23 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ITeam.DotnetCore.Faker;
 using ITeam.DotnetCore.FakeServices;
 using ITeam.DotnetCore.IServices;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 using ITeam.DotnetCore.WebApi.Middlewares;
 using FluentValidation.AspNetCore;
-using FluentValidation;
-using ITeam.DotnetCore.Models;
 using ITeam.DotnetCore.Models.Validators;
 
 namespace ITeam.DotnetCore.WebApi
